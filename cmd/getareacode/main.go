9d6050961545d79c7cd324e897d17f29b598e071
liuzhanliang <zhanliangliu@qq.com>
2019-07-20 18:41:22 +0800

    log -> github.com/golang/glog

--- cmd/getareacode/main.go
+++ cmd/getareacode/main.go
@@ -2,28 +2,30 @@ package main
 
 import (
 	"fmt"
-	"log"
 
+	"github.com/golang/glog"
 	"zliu.org/goutil"
 )
 
 func main() {
 	p, err := pool.GetParser("addr", false)
 	if err != nil {
-		log.Fatal(err)
+		glog.Fatal(err)
 	}
 	fmt.Println(p.ExampleUrl)
-	tasks, _, err := Parse("addr", p.ExampleUrl)
+	years, _, err := Parse("addr", p.ExampleUrl)
 	if err != nil {
-		log.Fatal(err)
+		glog.Fatal(err)
 	}
-	for _, task := range tasks {
+	for _, task := range years {
 		fmt.Println(task.Url)
 		ret, _ := goutil.RegexpParse(task.Url, `\d+`)
 		if len(ret) != 1 {
-			log.Fatal(task.Url, " error")
+			glog.Fatal(task.Url, " error")
 		}
 		year := ret[0]
 		fmt.Println(year)
+		tasks, items, err := Parse(task.ParserName, task.Url)
+		fmt.Println(items, tasks, err)
 	}
 }
