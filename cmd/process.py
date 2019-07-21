7e210fdc781b517d1db5cf729c270df5f4d91112
liuzhanliang <zhanliangliu@qq.com>
2019-07-21 19:05:48 +0800

    update process.py

--- cmd/process.py
+++ cmd/process.py
@@ -14,21 +14,16 @@ def x(item, name):
     else:
         print("%s\t%s\t%s" % (name, item[name]['code'], item[name]['name']))
 
-def city(item): x(item, 'city')
-def county(item): x(item, 'county')
-def town(item): x(item, 'town')
-def village(item): x(item, 'village')
-
 def main(f):
     for line in open(f):
         if line.strip() == "": continue
         item = json.loads(line)
         cls = item['from_parser_']
         if cls == "addr_year": province(item)
-        elif cls == "province": city(item)
-        elif cls == "city": county(item)
-        elif cls == "county": town(item)
-        elif cls == "town": village(item)
+        elif cls == "province": x(item, 'city')
+        elif cls == "city": x(item, 'county')
+        elif cls == "county": x(item, 'town')
+        elif cls == "town": x(item, 'village')
 
 if __name__ == "__main__":
     main("../20190721000000.dat")
