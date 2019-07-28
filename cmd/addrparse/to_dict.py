d76a0ab55a459f1f3056d37170b502f867ff2545
liuzhanliang <zhanliangliu@qq.com>
2019-07-28 19:27:30 +0800

    update to_dict.py

--- cmd/addrparse/to_dict.py
+++ cmd/addrparse/to_dict.py
@@ -1,33 +1,29 @@
 import gzip
 import json
 
-def process(f, ofile):
-    if f.endswith(".gz"): fp = gzip.open(f, 'rt')
-    else: fp = open(f, 'rt')
-    data = {}
+codelen = {2:"province", 4:"city", 6:"county", 9:"town", 12:"village"}
+
+def process(infile):
+    if infile.endswith(".gz"): fp = gzip.open(infile, 'rt')
+    else: fp = open(infile, 'rt')
+    out = {}
     for line in fp:
         item = line.strip().split('\t')
         if len(item) != 2:
             print("err line: %s" % line.strip())
             continue
+        f = "%s.csv" % codelen[len(item[0])]
+        if f not in out: out[f] = open(f, "w")
         for key in item[1].split(','):
-            if key not in data: data[key] = {"loc": []}
-            data[key]["loc"].append(item[0])
-    out = open(ofile, "w")
-    max_cnt = 0
-    key = ''
-    for k, v in data.items():
-        one = json.dumps({"k": k, "v": v}, ensure_ascii=False)
-        out.write(one+"\n")
-        if len(v['loc']) > max_cnt:
-            max_cnt = len(v['loc'])
-            key = k
-    out.close()
-    print("%s: %d" % (key, max_cnt))
+            key = key.strip()
+            if key == "": continue
+            out[f].write("%s,%s\n" % (key, item[0]))
+    for k, v in out.items():
+        v.close()
 
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) < 3:
-        print("Usage: python %s <in_file> <out_file>" % sys.argv[0])
+    if len(sys.argv) < 2:
+        print("Usage: python %s <in_file>" % sys.argv[0])
         sys.exit(1)
-    process(sys.argv[1], sys.argv[2])
+    process(sys.argv[1])
