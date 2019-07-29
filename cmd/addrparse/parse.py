b904d5838cf61165c950053d4aeaa4082e622d70
liuzhanliang <zhanliangliu@qq.com>
2019-07-29 09:52:34 +0800

    addr parse

--- cmd/addrparse/parse.py
+++ cmd/addrparse/parse.py
@@ -1,11 +1,20 @@
 import requests
+import json
 
 url = "http://localhost:8080/loc/multimaxmatch"
+
+def process(text):
+    ret = requests.get(url, params={"text": text})
+    item = json.loads(ret.text)
+    if item['status'] != 'OK': return item['status']
+    msg = item['message']
+    for k, v in msg.items():
+        print(k,v['value'])
 
 if __name__ == "__main__":
     import sys
     if len(sys.argv) < 2:
         print("Usage: python %s <text>" % sys.argv[0])
         sys.exit(1)
-    ret = requests.get(url, params={"text": sys.argv[1]})
-    print(ret.text)
+    ret = process(sys.argv[1])
+    print(ret)
