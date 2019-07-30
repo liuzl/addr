5e53202669fe05fb553e8a410dcccdaf2e658a4c
liuzhanliang <zhanliangliu@qq.com>
2019-07-30 08:48:11 +0800

    从http接口获取一个地址码的标准名称

--- cmd/addrparse/parse.py
+++ cmd/addrparse/parse.py
@@ -1,15 +1,26 @@
 import requests
 import json
+import gzip
 
 url = "http://localhost:8080/loc/multimaxmatch"
+code_url = "http://127.0.0.1:5000/?code=%s"
 
 def process(text):
     ret = requests.get(url, params={"text": text})
     item = json.loads(ret.text)
     if item['status'] != 'OK': return item['status']
     msg = item['message']
+    addr = {}
     for k, v in msg.items():
-        print(k,v)
+        #print(k, v)
+        for addr_type, value in v['value'].items():
+            if addr_type not in addr:
+                addr[addr_type] = []
+            addr[addr_type].append({"code":value, "name":k, "pos":v['hits']})
+    print(addr)
+    if "province" in addr:
+        text = requests.get(code_url % addr['province'][0]['code'][0]).text
+        print(json.loads(text))
 
 if __name__ == "__main__":
     import sys
