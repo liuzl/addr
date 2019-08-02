d34dceba84e49846f41c20d454124fa8f02d2922
liuzhanliang <zhanliangliu@qq.com>
2019-08-02 16:16:41 +0800

    mimetype="application/json"

--- cmd/addrparse/parse_server.py
+++ cmd/addrparse/parse_server.py
@@ -2,7 +2,8 @@ import requests
 import json
 import gzip
 from flask import Flask, request, Response
-app = Flask(__name__)
+#app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 kv = {}
 
@@ -99,13 +100,14 @@ def process(text):
     result.sort(key=lambda x:x['length'], reverse=True)
     return result
 
-@app.route("/")
+@app.route("/api")
 def get():
     r = request.args.get('text', '')
     if r == "":
         return Response(json.dumps({'status':"error", 'message':"empty input"}))
     result = process(r)
-    return Response(json.dumps({'status':"ok", 'message':result}))
+    return Response(json.dumps({'status':"ok", 'message':result}),
+            mimetype="application/json")
 
 @app.route("/code")
 def get_code():
@@ -117,5 +119,8 @@ def get_code():
         return Response(json.dumps({'status':"error", 'message':"%s not found" % r}))
     return Response(json.dumps({'status':"ok", 'message':kv[r]}))
 
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')#
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5001, debug=True)
